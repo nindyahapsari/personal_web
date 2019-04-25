718461271a5ae9890d3b203a3b48ce86a7506e0a
Nindya Hapsari <nindyahapsariiljas@yahoo.com>
2019-04-25 22:00:23 +0200

    update hover effect

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -25,58 +25,86 @@ class Projects extends Component{
                 
                 
                 <div className="project-card weather">
-                    <a href="https://github.com/nindyahapsari/WeatherApp_Sunshine" target="_blank" rel="noopener noreferrer">
-                        <img className="project-img" id="weatherapp" src={weather} alt="weather app screenshot" style={{width: 100, height: 200}} />
-                        </a>
+                    <img className="project-img" id="weatherapp" src={weather} alt="weather app screenshot" style={{width: 100, height: 200}} />
+                      <div className="overlay">
+                            <a href="https://github.com/nindyahapsari/WeatherApp_Sunshine" target="_blank" rel="noopener noreferrer">
+                            <i className="fab fa-github" id="githubProject"></i>
+                            <p className="click">click for source code</p>
+                            </a>
                         <div className="project-text" id="weatherapptext" >Weather App <br/> Android </div>
+                        </div>
                 </div>
 
 
 
                 <div className="project-card todo">
-                    <a href="https://github.com/nindyahapsari/Todo_List" target="_blank" rel="noopener noreferrer">
-                    <img className="project-img" id="todoapp" src={todo} alt="todo app screenshot" style={{width: 150, height: 200}}/>
-                    <div className="project-text todotxt" id="todoapptext">To-do App <br/> Android</div>
-                        </a>
+                <img className="project-img" id="todoapp" src={todo} alt="todo app screenshot" style={{width: 150, height: 200}}/>
+                    <div className="overlay">
+                            <a href="https://github.com/nindyahapsari/Todo_List" target="_blank" rel="noopener noreferrer">
+                            <i className="fab fa-github" id="githubProject" ></i>
+                            <p className="click">click for source code</p>
+                                <div className="project-text todotxt" id="todoapptext">To-do App <br/> Android</div>
+                            </a>
+                        </div>
                         
                 </div>
             
             
                 <div className="project-card plant">
-                    <a href="https://github.com/nindyahapsari/plants-id" target="_blank" rel="noopener noreferrer">
-                        <img className="project-img" id="plantapp" src={plantapp} style={{width: 200, height: 200}}  alt="plant detector app screenshot"/>
-                        <div className="project-text plant" id="plantdetectapptext">Plant Detector App <br/> Android</div>
-                        </a>
+                    <img className="project-img" id="plantapp" src={plantapp} style={{width: 200, height: 200}}  alt="plant detector app screenshot"/>
+                        <div className="overlay">
+                            <a href="https://github.com/nindyahapsari/plants-id" target="_blank" rel="noopener noreferrer">
+                            <i className="fab fa-github" id="githubProject" ></i>
+                            <p className="click">click for source code</p>
+                                <div className="project-text plant" id="plantdetectapptext">Plant Detector App <br/> Android</div>
+                            </a>
+                        </div>
                 </div>
             
                 <div className="project-card">
-                    <a href="https://github.com/nindyahapsari/personal_website" target="_blank" rel="noopener noreferrer">
-                        <img className="project-img" id="website" src={landing} style={{width: 300, height: 190}} alt="personal website screenshot"/>
-                        <div className="project-text personal" id="personalwebtext">Personal Website <br/> HTML CSS JS</div>
-                        </a>
-                </div>
-
-            
-                <div className="project-card">
-                    <a href="https://github.com/nindyahapsari/BubTeaApp" target="_blank" rel="noopener noreferrer">
-                        <img className="project-img" id="bubbleteaapp" src={bubtea} style={{width: 100, height: 200}} alt="bubbletea app screenshot"/>
-                        <div className="project-text bubtea" id="bubteaapptext">Ordering Bubble Tea App <br/> Android</div>
-                        </a>
-                </div>
-            
-            
-                <div className="project-card">
-                    <a href="https://github.com/nindyahapsari/random_quote_machine" target="_blank" rel="noopener noreferrer">
-                        <img className="project-img" id="rqm" src={rqm} style={{width: 300, height: 170}} alt="random quote machine screenshot"/>
-                        <div className="project-text " id="rqmtext">Random Quote Machine <br/> HTML CSS JS</div>
-                        </a> 
+                    <img className="project-img" id="website" src={landing} style={{width: 300, height: 190}} alt="personal website screenshot"/>
+                        <div className="overlay">
+                             <a href="https://github.com/nindyahapsari/personal_web" target="_blank" rel="noopener noreferrer">
+                             <i className="fab fa-github" id="githubProject" ></i>
+                             <p className="click">click for source code</p>
+                                <div className="project-text personal" id="personalwebtext">Personal Website <br/> HTML CSS JS(React)</div>
+                                </a>
+                        </div>
                 </div>
 
+            
                 <div className="project-card">
-                    <a href="https://codepen.io/nindyahapsari/full/xewbzr" target="_blank" rel="noopener noreferrer" >
-                        <img className="project-img" id="drum_machine" src={drum} style={{width: 300, height: 150}} alt="personal website screenshot"/>
+                    <img className="project-img" id="bubbleteaapp" src={bubtea} style={{width: 100, height: 200}} alt="bubbletea app screenshot"/>
+                        <div className="overlay">
+                            <a href="https://github.com/nindyahapsari/BubTeaApp" target="_blank" rel="noopener noreferrer">
+                                <i className="fab fa-github" id="githubProject" ></i>
+                                <p className="click">click for source code</p>
+                                <div className="project-text bubtea" id="bubteaapptext">Ordering Bubble Tea App <br/> Android</div>
+                             </a>
+                        </div>
+                </div>
+            
+            
+                <div className="project-card">
+                    <img className="project-img" id="rqm" src={rqm} style={{width: 300, height: 170}} alt="random quote machine screenshot"/>
+                        <div className="overlay">
+                             <a href="https://github.com/nindyahapsari/random_quote_machine" target="_blank" rel="noopener noreferrer">
+                             <i class="fab fa-github" id="githubProject" ></i>
+                             <p className="click">click for source code</p>
+                                    <div className="project-text " id="rqmtext">Random Quote Machine <br/> HTML CSS JS</div>
+                                </a> 
+                        </div>
+                </div>
+
+                <div className="project-card">
+                    <img className="project-img" id="drum_machine" src={drum} style={{width: 300, height: 150}} alt="personal website screenshot"/>
+                                <div className="overlay">
+                                <a href="https://codepen.io/nindyahapsari/full/xewbzr" target="_blank" rel="noopener noreferrer" >
+                            <i className="fab fa-github" id="githubProject" ></i>
+                            <p className="click">click for source code</p>
                         <div className="project-text" id="dmtext">Drum Machine<br/> HTML CSS JS </div>
                         </a>
+                    </div>
                 </div>
 
             </div>
